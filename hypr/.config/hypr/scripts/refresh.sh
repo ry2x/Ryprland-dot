4b5da14bd86a621d55ccdeb20aaa68aa1cd06324
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-11-06 07:45:25 +0900

    REMOVE: eliminate process management from refresh script

--- hypr/.config/hypr/scripts/refresh.sh
+++ hypr/.config/hypr/scripts/refresh.sh
@@ -4,20 +4,4 @@
 #  ┛┗┗┛┻ ┛┗┗┛┗┛┛┗
 #
 
-# kill already running processes
-_ps=(waybar rofi swaync)
-for _prs in "${_ps[@]}"; do
-    if pidof "${_prs}" >/dev/null; then
-        pkill "${_prs}"
-    fi
-done
-
-# relaunch waybar
-sleep 1
-waybar &
-
-# relaunch swaync
-sleep 0.5
-swaync >/dev/null 2>&1 &
-
-exit 0
+"/home/haku/Profile/Ryprland-dot/waypaper/.config/waypaper/load_matugen.sh" --dark --skip
