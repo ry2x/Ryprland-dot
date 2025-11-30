dc88ce274914f0c43777d89bdb7d89f0d84c72f2
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-11-30 22:03:31 +0900

    REFACTOR: simplify script paths in refresh and waybarRestart

--- hypr/.config/hypr/scripts/waybarRestart.sh
+++ hypr/.config/hypr/scripts/waybarRestart.sh
@@ -5,7 +5,8 @@
 #
 
 # kill already running processes
-_ps=(waybar rofi swaync)
+#_ps=(waybar rofi swaync)
+_ps=(waybar rofi)
 for _prs in "${_ps[@]}"; do
     if pidof "${_prs}" >/dev/null; then
         pkill "${_prs}"
@@ -17,7 +18,7 @@ sleep 1
 waybar &
 
 # relaunch swaync
-sleep 0.5
-swaync >/dev/null 2>&1 &
+#sleep 0.5
+#swaync >/dev/null 2>&1 &
 
 exit 0
