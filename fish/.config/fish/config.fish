65ae05cd0e23d706db7cb0e7c04c45f63ae4a29b
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-11 05:41:29 +0900

    ADD: integrate mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,19 @@
 #  ┏┓┳┏┓┓┏  ┏┓┏┓┳┓┏┓┳┏┓
 #  ┣ ┃┗┓┣┫━━┃ ┃┃┃┃┣ ┃┃┓
 #  ┻ ┻┗┛┛┗  ┗┛┗┛┛┗┻ ┻┗┛
-#                      
+#
 
 ### EVERYTHING IS IN conf.d folder ###
-
-
 
 # FZF
 fzf --fish | source
 
 # ZOXIDE
 zoxide init fish | source
+
+# mise activation https://mise.jdx.dev/getting-started.html
+if status is-interactive
+    mise activate fish | source
+else
+    mise activate fish --shims | source
+end
