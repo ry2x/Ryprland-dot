0de137e4e59124400e7c3e6860818bbdabf4fe65
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-11 05:42:27 +0900

    STYLE: add space

--- fish/.config/fish/conf.d/50-appearance.fish
+++ fish/.config/fish/conf.d/50-appearance.fish
@@ -8,7 +8,7 @@ set -g fish_color_normal brwhite
 set -g fish_color_autosuggestion brblack
 set -g fish_color_command brgreen
 set -g fish_color_error brred
-set -g fish_color_param brwhite
+set -g fish_color_param brwhite2
 
 # Prompt (Starship)
 starship init fish | source
