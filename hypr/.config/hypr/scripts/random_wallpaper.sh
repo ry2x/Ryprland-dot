fa105666b4f80bfaa4fe66984200a077248bb9ec
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-31 04:03:58 +0900

    REFACTOR: streamline wallpaper selection logic

--- hypr/.config/hypr/scripts/random_wallpaper.sh
+++ hypr/.config/hypr/scripts/random_wallpaper.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# Set dir varialable
-wall_dir="$HOME/Pictures/wallpapers"
+wallpaper_path=$(grep "wallpaper =" $HOME/.config/waypaper/config.ini | cut -d '=' -f2- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
-wall_selection=$(find "$wall_dir" -type f -print0 | shuf -z -n 1 | xargs -0 basename)
+wallpaper_path="${wallpaper_path/#~/$HOME}"
 
-[[ -n "$wall_selection" ]] && waypaper --wallpaper "${wall_dir}/${wall_selection}"
+[[ -n "$wallpaper_path" ]] && waypaper --wallpaper "$wallpaper_path"
