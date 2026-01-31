f1e55e912c9ec0a7ab150cd3f6ccbc760e0beba7
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-31 22:45:53 +0900

    REFACTOR: update wallpaper path and backend settings

--- waypaper/.config/waypaper/load_matugen.sh
+++ waypaper/.config/waypaper/load_matugen.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 
-# Set dir varialable
+# ┳┓┏┓┓ ┏┓┏┓┳┓  ┳┳┓┏┓┏┳┓┳┳┏┓┏┓┳┓
+# ┣┫┣ ┃ ┃┃┣┫┃┃  ┃┃┃┣┫ ┃ ┃┃┃┓┣ ┃┃
+# ┛┗┗┛┗┛┗┛┛┗┻┛  ┛ ┗┛┗ ┻ ┗┛┗┛┗┛┛┗
+
+configDir="$HOME/.config/waypaper/config.ini"
 scriptsDir="$HOME/.config/hypr/scripts"
 
-wallpaper_path=$(grep "wallpaper =" $HOME/.config/waypaper/config.ini | cut -d '=' -f2- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-wallpaper_path="${wallpaper_path/#~/$HOME}"
-
-# SWWW Config
-FPS=60
-TYPE="any"
-DURATION=2
-BEZIER=".43,1.19,1,.4"
-SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
-
-# initiate swww if not running
-swww query || swww-daemon --format xrgbz
-
-# Set wallpaper
-[[ -n "$wallpaper_path" ]] && swww img "${wallpaper_path}" $SWWW_PARAMS
-
 # Run matugen script
-[[ -n "$wallpaper_path" ]] && "$scriptsDir/matugenMagick.sh" --dark
-
-# Reload waybar swaync
-#[[ -n "$wallpaper_path" ]] && "$scriptsDir/waybarRestart.sh"
+[[ -n "$configDir" ]] && "$scriptsDir/matugenMagick.sh" --dark
