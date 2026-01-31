de5b9addfc54c0c55674345948d36b9db9142f10
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-31 22:45:46 +0900

    REFACTOR: update wallpaper change interval and logic

--- hypr/.config/hypr/scripts/random_wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wallpaper_path=$(grep "wallpaper =" $HOME/.config/waypaper/config.ini | cut -d '=' -f2- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-
-wallpaper_path="${wallpaper_path/#~/$HOME}"
-
-[[ -n "$wallpaper_path" ]] && waypaper --wallpaper "$wallpaper_path"
