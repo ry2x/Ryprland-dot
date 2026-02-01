c8cd496680b0d68d7f9bb82b98691c7e0c13db5e
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-02-02 05:51:00 +0900

    REFACTOR: streamline matugenMagick script and configs

--- waypaper/.config/waypaper/load_matugen.sh
+++ waypaper/.config/waypaper/load_matugen.sh
@@ -4,8 +4,4 @@
 # ┣┫┣ ┃ ┃┃┣┫┃┃  ┃┃┃┣┫ ┃ ┃┃┃┓┣ ┃┃
 # ┛┗┗┛┗┛┗┛┛┗┻┛  ┛ ┗┛┗ ┻ ┗┛┗┛┗┛┛┗
 
-configDir="$HOME/.config/waypaper/config.ini"
-scriptsDir="$HOME/.config/hypr/scripts"
-
-# Run matugen script
-[[ -n "$configDir" ]] && "$scriptsDir/matugenMagick.sh" --dark
+$HOME/.config/hypr/scripts/matugenMagick.sh --dark
