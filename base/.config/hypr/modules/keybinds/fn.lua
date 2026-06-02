3a9bb0de49794d29675b318952beebe35efdaa40
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-06-03 03:56:27 +0900

    ♻️ REFAC: enhance function key bindings and layer
    - Added function layer for media control and brightness.
    - Included new key bindings for brightness and player controls.
    - Improved notification messages for layer activation and deactivation.

--- base/.config/hypr/modules/keybinds/fn.lua
+++ base/.config/hypr/modules/keybinds/fn.lua
@@ -1,6 +1,7 @@
 -- Keybinds for function keys, media control, and brightness
 local P = require("modules.keybinds.constants")
 local hyprScript = P.hyprScript
+local mod = P.mod
 
 local fn_binds = {
     -- Volume, mic, brightness
@@ -23,3 +24,50 @@ local fn_binds = {
 for _, bind in ipairs(fn_binds) do
     hl.bind(bind[1], hl.dsp.exec_cmd(bind[2]), { description = bind[3] })
 end
+
+-- function submap for keyboard which has no default keybinds for media control and brightness
+hl.bind(mod .. " + F1",
+    function()
+        hl.dispatch(hl.dsp.submap("fnlayer"))
+        hl.dispatch(hl.dsp.exec_cmd("notify-send -e -u low -i \"" ..
+            P.icon .. "/fn_key_filled.png\" 'Function Layer Activated'"))
+    end,
+    { description = "Function Layer" }
+)
+
+local function_binds = {
+    -- brightness F1 F2
+    { "F1",  hyprScript .. "/backlight.sh --dec", "Brightness Down" },
+    { "F2",  hyprScript .. "/backlight.sh --inc", "Brightness Up" },
+
+    -- volume F10 F11 F12
+    { "F10", hyprScript .. "/volume.sh --toggle", "Volume Mute" },
+    { "F11", hyprScript .. "/volume.sh --dec",    "Volume Down" },
+    { "F12", hyprScript .. "/volume.sh --inc",    "Volume Up" },
+
+    -- playerctl F7 F8 F9
+    { "F7",  "playerctl previous",                "Play previous" },
+    { "F8",  "playerctl play-pause",              "Play/Pause" },
+    { "F9",  "playerctl next",                    "Play next" }
+}
+
+hl.define_submap("fnlayer",
+    function()
+        for _, bind in ipairs(function_binds) do
+            hl.bind(bind[1],
+                function()
+                    hl.dispatch(hl.dsp.exec_cmd(bind[2]))
+                end,
+                { description = bind[3] }
+            )
+        end
+        hl.bind("escape",
+            function()
+                hl.dispatch(hl.dsp.submap("reset"))
+                hl.dispatch(hl.dsp.exec_cmd("notify-send -e -u low -i \"" ..
+                    P.icon .. "/fn_key_outline.png\" 'Function Layer Deactivated'"))
+            end,
+            { description = "Back to Default Layer" }
+        )
+    end
+)
