1570c79b9e85b99c5d2586300ce0413ff21b4cc5
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-06-13 16:46:46 +0900

    ♻️ REFAC: Refactor Volume and WindowBorder elements
    - Improved code readability with consistent indentation.
    - Simplified logic in volume slider and window border rendering.
    - Enhanced maintainability by organizing methods and properties.

--- base/.config/mpv/scripts/uosc/elements/ManagedButton.lua
+++ base/.config/mpv/scripts/uosc/elements/ManagedButton.lua
@@ -8,29 +8,30 @@ local ManagedButton = class(Button)
 ---@param id string
 ---@param props ManagedButtonProps
 function ManagedButton:new(id, props) return Class.new(self, id, props) --[[@as ManagedButton]] end
+
 ---@param id string
 ---@param props ManagedButtonProps
 function ManagedButton:init(id, props)
-	---@type string | table | nil
-	self.command = nil
-	---@type boolean
-	self.hide = nil
-	---@type fun(hide: boolean) | nil
-	self.on_hide = nil
-	Button.init(self, id, table_assign({}, props, {on_click = function() execute_command(self.command) end}))
-	self:update(buttons:get(props.name))
-	self:register_disposer(buttons:subscribe(props.name, function(data) self:update(data) end))
+    ---@type string | table | nil
+    self.command = nil
+    ---@type boolean
+    self.hide = nil
+    ---@type fun(hide: boolean) | nil
+    self.on_hide = nil
+    Button.init(self, id, table_assign({}, props, { on_click = function() execute_command(self.command) end }))
+    self:update(buttons:get(props.name))
+    self:register_disposer(buttons:subscribe(props.name, function(data) self:update(data) end))
 end
 
 function ManagedButton:update(data)
-	local hide_before = self.hide
-	for _, prop in ipairs({'icon', 'active', 'badge', 'command', 'tooltip', 'hide'}) do
-		self[prop] = data[prop]
-	end
-	self.is_clickable = self.command ~= nil
-	if self.hide ~= hide_before and self.on_hide then
-		self.on_hide(self.hide)
-	end
+    local hide_before = self.hide
+    for _, prop in ipairs({ 'icon', 'active', 'badge', 'command', 'tooltip', 'hide' }) do
+        self[prop] = data[prop]
+    end
+    self.is_clickable = self.command ~= nil
+    if self.hide ~= hide_before and self.on_hide then
+        self.on_hide(self.hide)
+    end
 end
 
 return ManagedButton
