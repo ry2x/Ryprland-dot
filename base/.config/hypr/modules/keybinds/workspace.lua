bdb9752db32d004d814cace2b8292b4b4d32f71d
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-07-01 04:04:39 +0900

    ✨ FEAT: Implement column size management per workspace
    - Added functionality to change column size using keybinds.
    - Maintains column size state for each workspace.
    - Notifications for column size changes are included.

--- base/.config/hypr/modules/keybinds/workspace.lua
+++ base/.config/hypr/modules/keybinds/workspace.lua
@@ -3,6 +3,9 @@ local P = require("modules.constants")
 local mod = P.mod
 local smw = P.smw
 
+local F = require("modules.utils")
+local sendNotification = F.sendNotification
+
 for i = 1, 5 do
     local key = tostring(i)
     -- move to workspace
@@ -13,6 +16,9 @@ for i = 1, 5 do
         { description = "Move window to workspace " .. key })
 end
 
+hl.bind(mod .. "+ CAPS + Up", smw.workspace("-1"), { description = "Switch to -1 workspace" })
+hl.bind(mod .. "+ CAPS + Down", smw.workspace("+1"), { description = "Switch to +1 workspace" })
+
 -- toggle workspace overview
 hl.bind(mod .. " + SHIFT + TAB",
     function()
@@ -21,4 +27,63 @@ hl.bind(mod .. " + SHIFT + TAB",
         end
     end,
     { description = "Toggle workspace overview" }
+)
+
+-- change col size
+local col_state_tb = {}
+local BIG = 0.95
+local SMALL = 0.5
+
+local function isExistingInTb(ws)
+    if col_state_tb[ws] ~= nil then
+        return true
+    else
+        return false
+    end
+end
+
+local function setColSizeTb(ws, size, size_str)
+    col_state_tb[ws] = size
+    hl.dispatch(hl.dsp.layout("colresize all " .. size))
+    hl.config({ scrolling = { column_width = size } })
+    sendNotification(P.icon .. "/col_resize_" .. size_str .. ".png", "Column Size: Small", "")
+end
+
+hl.bind(mod .. "+ CAPS + TAB",
+    function()
+        local current_ws = hl.get_active_workspace()
+        if not current_ws then
+            return
+        end
+        local ws = current_ws.id
+
+        if not isExistingInTb(ws) then
+            setColSizeTb(ws, SMALL, "small")
+            return
+        end
+
+        if col_state_tb[ws] - 0.7 > 0 then
+            setColSizeTb(ws, SMALL, "small")
+        else
+            setColSizeTb(ws, BIG, "big")
+        end
+    end,
+    { description = "Change column size(0.5/0.95)" }
+)
+
+-- keep col size per workspace
+hl.on("workspace.active",
+    function()
+        local current_ws = hl.get_active_workspace()
+        if not current_ws then
+            return
+        end
+        local ws = current_ws.id
+
+        if not isExistingInTb(ws) then
+            hl.config({ scrolling = { column_width = BIG } })
+        else
+            hl.config({ scrolling = { column_width = col_state_tb[ws] } })
+        end
+    end
 )
