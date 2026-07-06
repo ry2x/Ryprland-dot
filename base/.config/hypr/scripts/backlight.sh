e600da3221a13cda318dfa2423758a483fd8bfbf
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-07-07 04:33:14 +0900

    🐛 FIX: Correct notify-send icon parameter
    Fix the icon parameter in the notify_user function to use the correct option.

--- base/.config/hypr/scripts/backlight.sh
+++ base/.config/hypr/scripts/backlight.sh
@@ -53,7 +53,7 @@ get_icon() {
 
 # Notify
 notify_user() {
-    notify-send -e -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Brightness : $current%"
+    notify-send -e -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -n "$icon" "Brightness : $current%"
 }
 
 # Change brightness
