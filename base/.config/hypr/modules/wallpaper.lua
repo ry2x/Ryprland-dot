931b97ff704801163869c1808e23d8230cb36454
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-08-08 22:15:35 +0900

    fix: change loop time to 60mins

--- base/.config/hypr/modules/wallpaper.lua
+++ base/.config/hypr/modules/wallpaper.lua
@@ -9,7 +9,7 @@ M.timer = hl.timer(
         hl.dispatch(hl.dsp.exec_cmd("theme-switch.sh random"))
     end,
     {
-        timeout = 360000,
+        timeout = 3600000,
         type = "repeat"
     }
 )
