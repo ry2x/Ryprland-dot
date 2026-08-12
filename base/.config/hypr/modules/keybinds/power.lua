d38b930408f3109bb3b5e9a1e6a3ef34b4a84e1c
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-08-12 10:30:13 +0900

    feat: replace power menu with rystal-shell

--- base/.config/hypr/modules/keybinds/power.lua
+++ base/.config/hypr/modules/keybinds/power.lua
@@ -2,6 +2,6 @@
 local P = require("modules.constants")
 local mod = P.mod
 
-hl.bind(mod .. " + X", hl.dsp.exec_cmd(P.hyprScript .. "/wlogout.sh"), { description = "⏻ PowerMenu" })
+hl.bind(mod .. " + X", hl.dsp.exec_cmd("ags request -i rystal-shell toggle-power-menu"), { description = "⏻ PowerMenu" })
 hl.bind(mod .. " + L", hl.dsp.exec_cmd("hyprlock"), { description = " Lock screen" })
 hl.bind("CTRL + ALT + Delete", hl.dsp.exit(0), { description = "⏻ Exit Hyprland" })
