805c2ee7de027240a61d2b519b67127eafd6b5ba
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-09-17 07:04:25 +0900

    feat: update function key bindings for media control
    - Replaced getHyprScript calls with direct commands.
    - Improved consistency in volume and brightness controls.
    - Ensured all media control keys are properly mapped.

--- base/.config/hypr/modules/keybinds/fnkey.lua
+++ base/.config/hypr/modules/keybinds/fnkey.lua
@@ -3,25 +3,24 @@ local P = require("modules.constants")
 local mod = P.mod
 
 local F = require("modules.utils")
-local getHyprScript = F.getHyprScript
 local sendNotification = F.sendNotification
 
 local fn_binds = {
     -- Volume, mic, brightness
-    { "XF86AudioRaiseVolume",  getHyprScript("volume.sh --inc"),        "Volume Up" },
-    { "XF86AudioLowerVolume",  getHyprScript("volume.sh --dec"),        "Volume Down" },
-    { "XF86AudioMute",         getHyprScript("volume.sh --toggle"),     "Volume Mute" },
-    { "XF86AudioMicMute",      getHyprScript("volume.sh --toggle-mic"), "Mic Mute" },
-    { "ALT + M",               getHyprScript("volume.sh --mic-inc"),    "Mic Volume Up" },
-    { "ALT + SHIFT + M",       getHyprScript("volume.sh --mic-dec"),    "Mic Volume Down" },
-    { "XF86MonBrightnessUp",   "ags request -i rystal-shell brightness up",     "Brightness Up" },
-    { "XF86MonBrightnessDown", "ags request -i rystal-shell brightness down",     "Brightness Down" },
+    { "XF86AudioRaiseVolume",  "ags request -i rystal-shell volume up",            "Volume Up" },
+    { "XF86AudioLowerVolume",  "ags request -i rystal-shell volume down",          "Volume Down" },
+    { "XF86AudioMute",         "ags request -i rystal-shell volume toggle",        "Volume Mute" },
+    { "XF86AudioMicMute",      "ags request -i rystal-shell mic toggle",           "Mic Mute" },
+    { "ALT + M",               "wpctl set-volume -l 1 @DEFAULT_AUDIO_SOURCE@ 5%+", "Mic Volume Up" },
+    { "ALT + SHIFT + M",       "wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 5%-",      "Mic Volume Down" },
+    { "XF86MonBrightnessUp",   "ags request -i rystal-shell brightness up",        "Brightness Up" },
+    { "XF86MonBrightnessDown", "ags request -i rystal-shell brightness down",      "Brightness Down" },
 
     -- Media control
-    { "XF86AudioNext",         "playerctl next",                        "Play next" },
-    { "XF86AudioPause",        "playerctl play-pause",                  "Play/Pause" },
-    { "XF86AudioPlay",         "playerctl play-pause",                  "Play/Pause" },
-    { "XF86AudioPrev",         "playerctl previous",                    "Play previous" }
+    { "XF86AudioNext",         "playerctl next",                                   "Play next" },
+    { "XF86AudioPause",        "playerctl play-pause",                             "Play/Pause" },
+    { "XF86AudioPlay",         "playerctl play-pause",                             "Play/Pause" },
+    { "XF86AudioPrev",         "playerctl previous",                               "Play previous" }
 }
 
 for _, bind in ipairs(fn_binds) do
@@ -40,17 +39,17 @@ hl.bind(mod .. " + F1",
 local function_binds = {
     -- brightness F1 F2
     { "F1",  "ags request -i rystal-shell brightness down", "Brightness Down" },
-    { "F2",  "ags request -i rystal-shell brightness up", "Brightness Up" },
+    { "F2",  "ags request -i rystal-shell brightness up",   "Brightness Up" },
 
     -- volume F10 F11 F12
-    { "F10", getHyprScript("volume.sh --toggle"), "Volume Mute" },
-    { "F11", getHyprScript("volume.sh --dec"),    "Volume Down" },
-    { "F12", getHyprScript("volume.sh --inc"),    "Volume Up" },
+    { "F10", "ags request -i rystal-shell volume toggle",   "Volume Mute" },
+    { "F11", "ags request -i rystal-shell volume down",     "Volume Down" },
+    { "F12", "ags request -i rystal-shell volume up",       "Volume Up" },
 
     -- playerctl F7 F8 F9
-    { "F7",  "playerctl previous",                "Play previous" },
-    { "F8",  "playerctl play-pause",              "Play/Pause" },
-    { "F9",  "playerctl next",                    "Play next" }
+    { "F7",  "playerctl previous",                          "Play previous" },
+    { "F8",  "playerctl play-pause",                        "Play/Pause" },
+    { "F9",  "playerctl next",                              "Play next" }
 }
 
 hl.define_submap("fnlayer",
